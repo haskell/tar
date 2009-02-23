91a10c3b15687847aacfa719512ce964fc5e785f
Duncan Coutts <duncan@haskell.org>
2009-02-23 11:31:32 +0000

    Update htar demo program
    Simplify and use the new tar api

--- htar/htar.hs
+++ htar/htar.hs
@@ -1,177 +1,116 @@
 module Main where
 
-import Codec.Archive.Tar
+import qualified Codec.Archive.Tar as Tar
 
-import Codec.Compression.GZip.GUnZip (gunzip)
+import qualified Codec.Compression.GZip as GZip (compress, decompress)
+import qualified Codec.Compression.BZip as BZip (compress, decompress)
 
 import qualified Data.ByteString.Lazy as BS
-import Data.ByteString.Lazy (ByteString)
-import Control.Monad
-import Data.Bits
-import Data.Char (toUpper)
-import qualified Data.Set as Set
-import System.Console.GetOpt
-import System.Environment
-import System.Exit
-import System.Locale (defaultTimeLocale)
-import System.IO
-import System.Posix.Types (EpochTime)
-import System.Time (ClockTime(..), toUTCTime, formatCalendarTime)
+import Data.ByteString.Lazy  (ByteString)
+import Data.Bits             (testBit)
+import Data.Char             (toUpper)
+import System.Console.GetOpt (OptDescr(..),ArgDescr(..), ArgOrder(..),
+                              getOpt', usageInfo)
+import System.Environment    (getArgs)
+import System.Exit           (exitFailure)
+import System.Locale         (defaultTimeLocale)
+import System.IO             (hPutStrLn, stderr)
+import System.Time           (ClockTime(..), toUTCTime, formatCalendarTime)
 
 main :: IO ()
-main = do args <- getArgs
-          (opts, files) <- parseOptions args
-          mainOpts opts files
+main = do
+  (opts, files) <- parseOptions =<< getArgs
+  main' opts files
 
-parseOptions :: [String] -> IO (Options, [FilePath])
-parseOptions args = 
-   do let (fs, files, nonopts, errs) = getOpt' RequireOrder optDescr args
-      when (not (null errs)) $ die errs
-      case nonopts of
-        []         -> return $ (foldl (flip ($)) defaultOptions fs, files)
-        ["--help"] -> usage
-        _          -> die (map (("unrecognized option "++).show) nonopts)
+main' :: Options -> [FilePath] -> IO ()
+main' (Options { optFile        = file,
+                 optAction      = action,
+                 optCompression = compression,
+                 optVerbosity   = verbosity }) files =
+  case action of
+    NoAction -> die ["No action given. Specify one of -c, -t or -x."]
+    Help     -> printUsage
+    Create   -> output . compress compression
+                       . Tar.write =<< Tar.pack "." (head files)
+                       --FIXME: don't use head here, allow all files
+    Extract  -> Tar.unpack "." . Tar.read . decompress compression =<< input
+    List     -> printEntries . Tar.read . decompress compression =<< input
+  where
+    input  = if file == "-" then BS.getContents else BS.readFile  file
+    output = if file == "-" then BS.putStr      else BS.writeFile file
 
-mainOpts :: Options -> [FilePath] -> IO ()
-mainOpts (Options { optAction = Nothing }) _ 
-    = die ["No action given. Specify one of -c, -t or -x."]
-mainOpts (Options { optFile = file, 
-                    optAction = Just action,
-                    optCompression = compression, 
-                    optVerbose = verbose }) files = 
-    -- FIXME: catch errors and print out nicely
-    case action of 
-      Create  -> recurseDirectories files 
-                 >>= mapM (createEntry verbose) 
-                 >>= output . compress compression 
-                         . writeTarArchive . TarArchive
-      Extract -> inputEntries >>= mapM_ (extractEntry verbose)
-      List    -> inputEntries >>= mapM_ (putStrLn . entryInfo verbose)
-  where input  = if file == "-" then BS.getContents else BS.readFile file
-        output = if file == "-" then BS.putStr      else BS.writeFile file
-        inputEntries = input >>= return . readEntries files . decompress compression
+    printEntries = Tar.foldEntries (\entry rest -> printEntry entry >> rest)
+                                   (return ()) fail
+    printEntry = putStrLn . entryInfo verbosity
 
-compress :: Maybe Compression -> ByteString -> ByteString
-compress Nothing      = id
-compress (Just GZip)  = error "gzip compression is not supported yet"
-compress (Just BZip2) = error "bzip2 compression is not supported yet"
-
-decompress :: Maybe Compression -> ByteString -> ByteString
-decompress Nothing      = id
-decompress (Just GZip)  = gunzip
-decompress (Just BZip2) = error "bzip2 decompression is not supported yet"
-
-readEntries :: [FilePath] -> ByteString -> [TarEntry]
-readEntries files = (if null files then id else keepFiles files) 
-                    . archiveEntries . readTarArchive
-
--- FIXME: allow files names to differ in trailing slashes
-keepFiles :: [FilePath] -> [TarEntry] -> [TarEntry]
-keepFiles files = filter (p . entryHeader)
-  where p = ((`Set.member` Set.fromList files) . tarFileName)
-
-createEntry :: Bool -> FilePath -> IO TarEntry
-createEntry verbose file =
-    do when verbose $ putStrLn file
-       createTarEntry file
-
-extractEntry :: Bool -> TarEntry -> IO ()
-extractEntry verbose e =
-    do when verbose $ putStrLn $ tarFileName $ entryHeader e
-       extractTarEntry e
-
-die :: [String] -> IO a
-die errs = do mapM_ (\e -> hPutStrLn stderr $ "htar: " ++ e) $ errs
-              hPutStrLn stderr "Try `htar --help' for more information."
-              exitFailure
-
-usage :: IO a
-usage = do putStrLn (usageInfo hdr optDescr)
-           exitWith ExitSuccess
-  where hdr = unlines ["htar creates and extracts TAR archives.",
-                       "",
-                       "Usage: htar [OPTION ...] [FILE ...]"]
-
--- * Options
-
-data Options = Options 
-    {
-     optFile :: FilePath, -- "-" means stdin/stdout
-     optAction :: Maybe Action,
-     optCompression :: Maybe Compression,
-     optVerbose :: Bool
-    }
- deriving Show
-
-data Action = Create
-            | Extract
-            | List
+data Compression = None | GZip | BZip
   deriving Show
 
-data Compression = GZip | BZip2
-  deriving Show
+compress :: Compression -> ByteString -> ByteString
+compress None = id
+compress GZip = GZip.compress
+compress BZip = BZip.compress
 
-defaultOptions :: Options
-defaultOptions = Options {
-                          optFile = "-",
-                          optAction = Nothing,
-                          optCompression = Nothing,
-                          optVerbose = False
-                         }
+decompress :: Compression -> ByteString -> ByteString
+decompress None = id
+decompress GZip = GZip.decompress
+decompress BZip = BZip.decompress
 
-optDescr :: [OptDescr (Options -> Options)]
-optDescr = 
-    [
-     Option ['c'] ["create"] (action Create) "Create a new archive.",
-     Option ['x'] ["extract","get"] (action Extract) "Extract files.",
-     Option ['t'] ["list"] (action List) "List archive contents.",
-     Option ['z'] ["gzip","ungzip"] (compression GZip) "Use gzip compression.",
-     Option ['j'] ["bzip2"] (compression BZip2) "Use bzip2 compression.",
-     Option ['f'] ["file"] (ReqArg (\f o -> o { optFile = f}) "ARCHIVE")
-            "Use archive file ARCHIVE.",
-     Option ['v'] ["verbose"] (NoArg (\o -> o { optVerbose = True }))
-            "Increase output verbosity."
-    ]
- where action a = NoArg (\o -> o { optAction = Just a })
-       compression c = NoArg (\o -> o { optCompression = Just c })
+data Verbosity = Verbose | Concise
 
--- * Formatted information about archives
+------------------------
+-- List archive contents
 
-entryInfo :: Bool -- ^ Verbosity
-          -> TarEntry -> String
-entryInfo True  = detailedInfo . entryHeader
-entryInfo False = tarFileName  . entryHeader
+entryInfo :: Verbosity -> Tar.Entry -> String
+entryInfo Verbose = detailedInfo
+entryInfo Concise = Tar.fileName
 
-detailedInfo :: TarHeader -> String
-detailedInfo hdr = unwords [typ:mode, owner, group, size, time, name++link]
-    where typ = case tarFileType hdr of
-                  TarHardLink        -> 'h'
-                  TarSymbolicLink    -> 'l'
-                  TarCharacterDevice -> 'c'
-                  TarBlockDevice     -> 'b'
-                  TarDirectory       -> 'd'
-                  TarFIFO            -> 'p'
-                  _                  -> '-'
-          mode = concat [u,g,o]
-              where m = tarFileMode hdr 
-                    f r w x s c = [if testBit m r then 'r' else '-',
-                                   if testBit m w then 'w' else '-',
-                                   if testBit m s 
-                                     then if testBit m x then c   else toUpper c
-                                     else if testBit m x then 'x' else '-']
-                    u = f 8 7 6 11 's'
-                    g = f 5 4 3 10 's'
-                    o = f 2 1 0  9 't'
-          owner = rpad 7 ' ' $ nameOrID (tarOwnerName hdr) (tarOwnerID hdr)
-          group = rpad 7 ' ' $ nameOrID (tarGroupName hdr) (tarGroupID hdr)
-          nameOrID n i = if null n then show i else n
-          size = lpad 11 ' ' $ show (tarFileSize hdr)
-          time = formatEpochTime "%Y-%m-%d %H:%M:%S" (tarModTime hdr)
-          name = tarFileName hdr
-          link = case tarFileType hdr of
-                   TarHardLink     -> " link to " ++ tarLinkTarget hdr
-                   TarSymbolicLink -> " -> " ++ tarLinkTarget hdr
-                   _               -> ""
+--TODO: format exactly like gnutar
+--      check with larger examples where we sometimes get misaligned columns
+detailedInfo :: Tar.Entry -> String
+detailedInfo entry =
+  unwords [ typ:mode
+          , owner ++ '/' : group
+          , size
+          , time
+          , name++link ]
+  where
+    typ = case Tar.fileType entry of
+            Tar.HardLink        -> 'h'
+            Tar.SymbolicLink    -> 'l'
+            Tar.CharacterDevice -> 'c'
+            Tar.BlockDevice     -> 'b'
+            Tar.Directory       -> 'd'
+            Tar.FIFO            -> 'p'
+            _                   -> '-'
+    mode = concat [u,g,o]
+        where m = Tar.fileMode entry
+              f r w x s c = [if testBit m r then 'r' else '-',
+                             if testBit m w then 'w' else '-',
+                             if testBit m s
+                               then if testBit m x then c   else toUpper c
+                               else if testBit m x then 'x' else '-']
+              u = f 8 7 6 11 's'
+              g = f 5 4 3 10 's'
+              o = f 2 1 0  9 't'
+    owner = rpad 7 ' ' $ nameOrID ownerName (Tar.ownerId entry)
+    group = rpad 7 ' ' $ nameOrID groupName (Tar.groupId entry)
+    ownerName = case Tar.headerExt entry of
+      Tar.UstarHeader { Tar.ownerName = n } -> n
+      Tar.GnuHeader   { Tar.ownerName = n } -> n
+      _                                      -> ""
+    groupName = case Tar.headerExt entry of
+      Tar.UstarHeader { Tar.groupName = n } -> n
+      Tar.GnuHeader   { Tar.groupName = n } -> n
+      _                                      -> ""
+    nameOrID n i = if null n then show i else n
+    size = lpad 4 ' ' $ show (Tar.fileSize entry)
+    time = formatEpochTime "%Y-%m-%d %H:%M" (Tar.modTime entry)
+    name = Tar.fileName entry
+    link = case Tar.fileType entry of
+             Tar.HardLink     -> " link to " ++ Tar.linkTarget entry
+             Tar.SymbolicLink -> " -> " ++ Tar.linkTarget entry
+             _                -> ""
 
 lpad :: Int -> a -> [a] -> [a]
 lpad n x xs = replicate (n - length xs) x ++ xs
@@ -179,10 +118,88 @@ lpad n x xs = replicate (n - length xs) x ++ xs
 rpad :: Int -> a -> [a] -> [a]
 rpad n x xs = xs ++ replicate (n - length xs) x
 
-formatEpochTime :: String -> EpochTime -> String
-formatEpochTime f = 
+formatEpochTime :: String -> Tar.EpochTime -> String
+formatEpochTime f =
     formatCalendarTime defaultTimeLocale f . toUTCTime . epochTimeToClockTime
 
-epochTimeToClockTime :: EpochTime -> ClockTime
+epochTimeToClockTime :: Tar.EpochTime -> ClockTime
 epochTimeToClockTime e = TOD s (truncate (1000000000 * f))
     where (s,f) = properFraction (toRational e)
+
+------------------------
+-- Command line handling
+
+data Options = Options {
+    optFile        :: FilePath, -- "-" means stdin/stdout
+    optAction      :: Action,
+    optCompression :: Compression,
+    optVerbosity   :: Verbosity
+  }
+
+defaultOptions :: Options
+defaultOptions = Options {
+    optFile        = "-",
+    optAction      = NoAction,
+    optCompression = None,
+    optVerbosity   = Concise
+  }
+
+data Action = NoAction
+            | Help
+            | Create
+            | Extract
+            | List
+  deriving Show
+
+optDescr :: [OptDescr (Options -> Options)]
+optDescr =
+  [ Option ['c'] ["create"]
+      (action Create)
+      "Create a new archive."
+  , Option ['x'] ["extract", "get"]
+      (action Extract)
+      "Extract files from an archive."
+  , Option ['t'] ["list"]
+      (action List)
+      "List the contents of an archive."
+  , Option ['f'] ["file"]
+      (ReqArg (\f o -> o { optFile = f}) "ARCHIVE")
+      "Use archive file ARCHIVE."
+  , Option ['z'] ["gzip", "gunzip", "ungzip"]
+      (compression GZip)
+      "Use gzip compression."
+  , Option ['j'] ["bzip2"]
+      (compression BZip)
+      "Use bzip2 compression."
+  , Option ['v'] ["verbose"]
+      (NoArg (\o -> o { optVerbosity = Verbose }))
+      "Verbosely list files processed."
+  , Option ['h', '?'] ["help"]
+      (action Help)
+      "Print this help output."
+  ]
+  where
+    action      a = NoArg (\o -> o { optAction = a })
+    compression c = NoArg (\o -> o { optCompression = c })
+
+printUsage :: IO ()
+printUsage = putStrLn (usageInfo headder optDescr)
+  where
+    headder = unlines ["htar creates and extracts TAR archives.",
+                       "",
+                       "Usage: htar [OPTION ...] [FILE ...]"]
+
+parseOptions :: [String] -> IO (Options, [FilePath])
+parseOptions args =
+  let (fs, files, nonopts, errors) = getOpt' RequireOrder optDescr args
+  in case (nonopts, errors) of
+       ([], [])    -> return $ (foldl (flip ($)) defaultOptions fs, files)
+       (_ , (_:_)) -> die errors
+       (_ ,  _)    -> die (map (("unrecognized option "++).show) nonopts)
+
+die :: [String] -> IO a
+die errs = do
+  mapM_ (\e -> hPutStrLn stderr $ "htar: " ++ e) $ errs
+  hPutStrLn stderr "Try `htar --help' for more information."
+  exitFailure
+
