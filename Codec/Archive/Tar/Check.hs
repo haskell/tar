2e896efe9042089e5e05aaee7777959692f532a3
Herbert Valerio Riedel <hvr@gnu.org>
2018-03-09 16:27:31 +0100

    Temporarily revert "Add offending path to TarBombError"
    This reverts commit 7a62a5a6f7b3181b7c941c5abd5dc7ef0bc3f474.

--- Codec/Archive/Tar/Check.hs
+++ Codec/Archive/Tar/Check.hs
@@ -132,19 +132,19 @@ checkEntryTarbomb _ entry | nonFilesystemEntry = Nothing
 checkEntryTarbomb expectedTopDir entry =
   case FilePath.Native.splitDirectories (entryPath entry) of
     (topDir:_) | topDir == expectedTopDir -> Nothing
-    _ -> Just $ TarBombError expectedTopDir (entryPath entry)
+    _ -> Just $ TarBombError expectedTopDir
 
 -- | An error that occurs if a tar file is a \"tar bomb\" that would extract
 -- files outside of the intended directory.
-data TarBombError = TarBombError FilePath FilePath
+data TarBombError = TarBombError FilePath
                   deriving (Typeable)
 
 instance Exception TarBombError
 
 instance Show TarBombError where
-  show (TarBombError expectedTopDir tarBombPath)
-    = "File in tar archive, " ++ show tarBombPath ++
-    ", is not in the expected directory " ++ show expectedTopDir
+  show (TarBombError expectedTopDir)
+    = "File in tar archive is not in the expected directory " ++ show expectedTopDir
+
 
 --------------------------
 -- Portability
