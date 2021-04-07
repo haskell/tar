ad8c0feaf55bb42ad9f95dae19e27c03117d232c
Julian Ospald <hasufell@posteo.de>
2021-04-07 14:23:34 +0200

    Avoid exposing These

--- Codec/Archive/Tar/Types.hs
+++ Codec/Archive/Tar/Types.hs
@@ -69,6 +69,7 @@ import qualified Data.ByteString       as BS
 import qualified Data.ByteString.Char8 as BS.Char8
 import qualified Data.ByteString.Lazy  as LBS
 import Control.DeepSeq
+import Control.Exception (Exception)
 
 import qualified System.FilePath as FilePath.Native
          ( joinPath, splitDirectories, addTrailingPathSeparator )
@@ -372,7 +373,8 @@ fromTarPathToWindowsPath (TarPath namebs prefixbs) = adjustDirectory $
 --
 toTarPath :: Bool -- ^ Is the path for a directory? This is needed because for
                   -- directories a 'TarPath' must always use a trailing @\/@.
-          -> FilePath -> These SplitError TarPath
+          -> FilePath
+          -> These SplitError TarPath
 toTarPath isDir = splitLongPath
                 . addTrailingSep
                 . FilePath.Posix.joinPath
@@ -384,6 +386,8 @@ toTarPath isDir = splitLongPath
 data SplitError = FileNameEmpty
                 | FileNameTooLong
                 deriving Show
+
+instance Exception SplitError
 
 
 -- | Take a sanitised path, split on directory separators and try to pack it
